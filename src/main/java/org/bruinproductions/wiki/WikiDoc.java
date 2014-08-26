c9ae958e492c1fb880fd2386047b1b6dda157d7c
Agoston Horvath <agoston@ripe.net>
2014-08-26 10:47:22 +0200

    added revision to wikiDoc

--- src/main/java/org/bruinproductions/wiki/WikiDoc.java
+++ src/main/java/org/bruinproductions/wiki/WikiDoc.java
@@ -2,11 +2,13 @@ package org.bruinproductions.wiki;
 
 public class WikiDoc<T> {
     private T document;
+    private int revision;
 
     public WikiDoc() {}
 
-    public WikiDoc(T document) {
+    public WikiDoc(T document, int revision) {
         this.document = document;
+        this.revision = revision;
     }
 
     public T getDocument() {
@@ -15,5 +17,13 @@ public class WikiDoc<T> {
 
     public void setDocument(T document) {
         this.document = document;
+    }
+
+    public int getRevision() {
+        return revision;
+    }
+
+    public void setRevision(int revision) {
+        this.revision = revision;
     }
 }
